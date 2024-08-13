816df10e1ae9c91d447fa4f3c64a7d1c286bb32d
Chim Saina <chimsaina982gmail.com>
2024-08-13 22:56:02 +0700

    update

--- src/components/cart/home-page-components/SlideImageComponent.jsx
+++ src/components/cart/home-page-components/SlideImageComponent.jsx
@@ -6,7 +6,7 @@ const SlideImageComponent = () => {
     <div className="my-[80px] mx-[100px] h-56 sm:h-64 xl:h-80 2xl:h-96">
       <Carousel slideInterval={3000}>
         <img
-          src="/image/image1/education2.jpg"
+          src="https://media.istockphoto.com/id/871927176/photo/repairman-disassemble-motherboard-with-screwdriver.jpg?s=612x612&w=0&k=20&c=uoGvYBkr5o_o1So1OBTVu8IS8jX79tUfgzz6_XBVO6w="
           alt="Slide 1"
           className="object-fit"
         />
@@ -21,12 +21,12 @@ const SlideImageComponent = () => {
           className="object-fit"
         />
         <img
-          src="/image/image1/education2.jpg"
+          src="https://media.istockphoto.com/id/871927176/photo/repairman-disassemble-motherboard-with-screwdriver.jpg?s=612x612&w=0&k=20&c=uoGvYBkr5o_o1So1OBTVu8IS8jX79tUfgzz6_XBVO6w="
           alt="Slide 4"
           className="object-fit"
         />
         <img
-          src="/image/image1/education2.jpg"
+          src="https://media.istockphoto.com/id/871927176/photo/repairman-disassemble-motherboard-with-screwdriver.jpg?s=612x612&w=0&k=20&c=uoGvYBkr5o_o1So1OBTVu8IS8jX79tUfgzz6_XBVO6w="
           alt="Slide 5"
           className="object-fit"
         />
